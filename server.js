9b929ba0fc17ef503d95a7f016bc299003db9a08
Obukwelu Ebuka Valentine <obukweluebuka@gmail.com>
2020-05-21 18:14:28 +0200

    fix error

--- server.js
+++ server.js
@@ -1,104 +1,116 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const afamefunaModel = require('./model/afamefuna');
-const bodyParser = require('body-parser');
-mongoose.connect(process.env.DB_CONN_STRING, { useNewUrlParser: true, dbName: "db-afamefuna" });
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const cron = require('cron').CronJob;
-const sample = require("./utils").sample
-const secure = require('express-force-https');
-var https = require('https');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const afamefunaModel = require("./model/afamefuna");
+const bodyParser = require("body-parser");
+mongoose.connect(process.env.DB_CONN_STRING, {
+  useNewUrlParser: true,
+  dbName: "db-afamefuna",
+});
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cron = require("cron").CronJob;
+const sample = require("./utils").sample;
+const secure = require("express-force-https");
+var https = require("https");
 const flash = require("connect-flash");
 
-const ghostContentAPI = require('@tryghost/content-api');
-const { adminRoute, authRoute, indexRoute } = require('./router')
-
+const ghostContentAPI = require("@tryghost/content-api");
+const { adminRoute, authRoute, indexRoute } = require("./router");
 
 const api = new ghostContentAPI({
   url: process.env.GHOST_URL, // remember to move to .env file
   key: process.env.GHOST_KEY,
-  version: "v3"
+  version: "v3",
 });
 
 const app = express();
 
 // app.use(secure)
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-  secret: process.env.SESSION_SECRETS,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false },
-  store: new MongoStore({ url: process.env.DB_CONN_STRING })
-
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRETS,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+    store: new MongoStore({ url: process.env.DB_CONN_STRING }),
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
 app.use(async function (req, res, next) {
-  let posts = await api.posts.browse({ limit: 10, include: 'tags,authors' });
-  res.locals.posts = posts;
-  res.locals.success_messages = req.flash('success_messages');
-  next();
+  try {
+    let posts = await api.posts.browse({ limit: 10, include: "tags,authors" });
+    res.locals.posts = posts;
+    res.locals.success_messages = req.flash("success_messages");
+  } catch (error) {
+    console.log(error);
+  } finally {
+    next();
+  }
 });
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
-
-
-
-app.get('/search/:query', function (req, res) {
+app.get("/search/:query", function (req, res) {
   afamefunaModel
     .aggregate([
-      { $match: { name: { $regex: req.params.query, $options: 'i' } } },
+      { $match: { name: { $regex: req.params.query, $options: "i" } } },
       {
         $project: {
           name: 1,
-          name_length: { $strLenCP: "$name" }
-        }
+          name_length: { $strLenCP: "$name" },
+        },
       },
       { $sort: { name_length: 1 } },
-      { $project: { name_length: 0 } }
-    ]).exec()
+      { $project: { name_length: 0 } },
+    ])
+    .exec()
     .catch(function (err) {
-      console.log("query error", err)
-    }).then(function (data) {
+      console.log("query error", err);
+    })
+    .then(function (data) {
       res.send({ success: true, results: [...data] });
     });
 });
 
-
-app.use("/", indexRoute)
+app.use("/", indexRoute);
 app.use("/admin", adminRoute);
 app.use("/auth", authRoute);
 
-app.get("*", function (req, res, next) { // catch all route handler
-  res.redirect("/")
-})
+app.get("*", function (req, res, next) {
+  // catch all route handler
+  res.redirect("/");
+});
 
-app.use(function (err, req, res, next) { // error handler
-  res.status(err.type)
-  res.render('error', { error: err.message })
-})
+app.use(function (err, req, res, next) {
+  // error handler
+  const err_code = err.type ? err.type : 404
+  res.status(err_code);
+  res.render("error", { error: err.message });
+});
 
 // cron job to create a sample of name from the afam collection.
 // future update would be to save the data in a redis cache.
 
-var job = new cron("* 59 23 * * *", function() {
-   sample();
-}, null, false)
+var job = new cron(
+  "* 59 23 * * *",
+  function () {
+    sample();
+  },
+  null,
+  false
+);
 
 job.start();
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('listening on port 3000');
+  console.log("listening on port 3000");
 });
-
-
